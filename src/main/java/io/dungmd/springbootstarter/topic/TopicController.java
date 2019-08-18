4cf25ce35b0ca663ad2127db66594396baf250a5
dungruoc <minhdung.dang@gmail.com>
2019-08-18 09:15:35 +0700

    19 - Creating Topic using POST

--- src/main/java/io/dungmd/springbootstarter/topic/TopicController.java
+++ src/main/java/io/dungmd/springbootstarter/topic/TopicController.java
@@ -3,16 +3,32 @@ package io.dungmd.springbootstarter.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
 
+    @Autowired
+    private TopicService topicService;
+    
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
-        return Arrays.asList(new Topic("spring", "Spring Framework", "Spring Framework Description"),
-                new Topic("java", "Core Java", "Core Java Description"),
-                new Topic("javascript", "JavaScript", "JavaScript Description"));
+        return topicService.getAllTopics();
+    }
+    
+    @GetMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+    
+    @PostMapping("/topics")
+    public void addTopic(@RequestBody Topic topic) {
+        topicService.addTopic(topic);
     }
 }
